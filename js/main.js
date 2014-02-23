59cd0f7fc7cca961b423ba970e90d62299100108
Ben Wells <benwellsmusic@gmail.com>
2014-02-23 00:00:52 -0500

    js animations, customized animations.css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('document').ready(function() {
+  var logo = $('#logo');
+  logo.addClass('fadeIn').parent();
+
+  setTimeout(function(){
+    logo.removeClass('fadeIn').parent().addClass('grow').mouseover(function (){
+      $(this).removeClass('fadeIn').addClass('grow'); 
+    });
+  },1500)
+});
