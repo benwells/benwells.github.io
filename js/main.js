680dc57671bacd9271345df5a89bdac34f080d23
Ben Wells <benwellsmusic@gmail.com>
2014-02-23 10:41:53 -0500

    updated main.js with lib object and conditional animation function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-$('document').ready(function() {
-  var logo = $('#logo');
-  logo.addClass('fadeIn').parent();
+var lib = {
+  init_animations_url: function (urlPattern) {
+    var path = window.location.href.replace('http://benwells.github.io','').replace('http://localhost:4000',''),
+        logo;
 
-  setTimeout(function(){
-    logo.removeClass('fadeIn').parent().addClass('grow').mouseover(function (){
-      $(this).removeClass('fadeIn').addClass('grow'); 
-    });
-  }, 1500);
+    if (urlPattern == path) {
+      logo = $('#logo').addClass('fadeIn');
+
+      setTimeout(function(){
+        logo.removeClass('fadeIn').parent().addClass('grow').mouseover(function (){
+          $(this).removeClass('fadeIn').addClass('grow'); 
+        });
+      }, 1500);
+    }
+  }
+};
+
+$('document').ready(function() {
+  lib.init_animations_url('/');
 });
