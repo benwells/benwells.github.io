8735b87f6613c61e5e3ca42bf62feea708d094dc
Ben Wells <benwellsmusic@gmail.com>
2014-02-23 15:58:49 -0500

    fade out click handler

--- js/main.js
+++ js/main.js
@@ -7,14 +7,27 @@ var lib = {
       logo = $('#logo').addClass('fadeIn');
 
       setTimeout(function(){
-        logo.removeClass('fadeIn').parent().addClass('grow').mouseover(function (){
-          $(this).removeClass('fadeIn').addClass('grow'); 
-        });
+        logo.removeClass('fadeIn').parent().addClass('grow');
       }, 1500);
     }
+  },
+  nav_pill_click_handler: function () {
+    $('a').click(function (e) {
+      var t    = this,
+          href = t.href;
+
+      e.preventDefault();
+      $(t).parent().parent().addClass('fadeOut');
+
+      //setTimeout to wait for fade out animation to complete before changing pages
+      setTimeout(function (){
+        window.location = href;
+      }, 200);
+    });  
   }
 };
 
 $('document').ready(function() {
   lib.init_animations_url('/');
+  lib.nav_pill_click_handler();
 });
