14bc2890bf9bb063e7d61257e3651e522e3a7017
Ben Wells <mrbrwells@gmail.com>
2015-03-01 17:45:02 -0500

    grunt add-tags task will now add a new tag slug and name to tags.yml

--- GruntFile.js
+++ GruntFile.js
@@ -10,13 +10,32 @@ module.exports = function(grunt) {
           reload: true,
         },
       },
+    },
+    concat: {
+      tagyml: {
+        options: {
+          process: function(src, filepath) {
+            var newLns = "- slug: " + grunt.config('tagSlug') + "\n" + "  name: " +
+              grunt.config('tagName') + "\n";
+            return src + newLns;
+          },
+        },
+        files: {
+          '_data/tags.yml': ['_data/tags.yml'],
+        },
+      },
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task(s).
   grunt.registerTask('default', ['watch']);
+  grunt.registerTask('add-tag', function (tagSlug, tagName) {
+    grunt.config.set('tagSlug', tagSlug);
+    grunt.config.set('tagName', tagName);
+    grunt.task.run('concat:tagyml');
+  });
 
 };
