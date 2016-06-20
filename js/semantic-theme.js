7b28531edcfeca766d98185516fe98146dc29e80
Ben W <mrbrwells@gmail.com>
2016-06-20 08:26:16 -0400

    wired up a lot of stuff, custom smooth scroll, now onto resume infographic portion

--- js/semantic-theme.js
+++ js/semantic-theme.js
@@ -1,28 +1,10 @@
-function doItToIt() {
-  console.log('fire');
-  // $(this).transition('vertical flip', '500ms').hide()//.transition('vertical flip', '600ms')
-  // $('.itembenn').transition('vertical flip', '500ms')//.transition('vertical flip', '600ms')
-    // .on('mouseenter', doItToIt)
-}
-
-function leave() {
-  $(this)
-    .transition('vertical flip in').on('mouseenter', doItToIt)
-}
-
 (function($) {
-  setTimeout(function() {
-    // $('.ui.sidebar')
-    // // .addClass('visible');
-    // .transition('fly up');
-    // $('.item').on(function(e) {
-    //   console.log('hover');
-    //   $(this).transition('vertical flip in').css('background', 'red');
-    // },function(e) {
-    //   $(this).transition('horizontal flip in').css('background', 'none');
-    // });
-    console.log('setTimeout');
-    $('.itemben').on('mouseenter', doItToIt);
-    // $('.item').on('mouseleave', leave);
-  }, 500)
+  // silky scroll
+  $('a.silky-scroll').on('click', function() {
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 400);
+    return false;
+  });
+
 })(jQuery);
